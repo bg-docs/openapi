d4e44ff6a903aaff7a14d66797a07ba5b0567cf0
wangzheng <wangzheng@qihangxingchen.com>
2022-11-09 11:01:33 +0800

    addpath

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@
 require './lib/unique_head.rb'
 
 # activate :i18n
-activate :i18n, :langs => [:zh,:tc,:en],:path => "/:locale/"
+activate :i18n, :langs => [:zh,:tc,:en],:path => "/openapi/:locale/"
 
 # Markdown
 set :markdown_engine, :redcarpet
